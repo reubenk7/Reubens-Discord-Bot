6ba0744fdc27c4d59a210050dfdbbd2b60453cf0
Reuben Knox <knore935@student.otago.ac.nz>
2022-06-22 16:24:57 +1200

    Discord bot first commit. Basic bot has been created

---
+++ index.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+
+// Node's file system and path modules
+const fs = require('node:fs');
+const path = require('node:path');
+
+// Require the necessary discord.js classes
+const { Client, Collection, Intents } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+
+// set commands path to the commands folder
+const commandsPath = path.join(__dirname, 'commands');
+// read command files filtering so only .js
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+client.once('ready', c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Command interactions
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
